ea2a3e2b10a4a2ace836373040c75a89e1ccb15c
outsung <40460655+outsung@users.noreply.github.com>
2019-12-18 14:29:19 +0900

    12.18

---
+++ precache-manifest.c3c6a4cabea5d8496ea178dc840fa885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9cf5d654839a16791f88d77e7a41f00",
+    "url": "/o3o-todolist/index.html"
+  },
+  {
+    "revision": "18933265addb825ab41b",
+    "url": "/o3o-todolist/static/css/main.6b6fd533.chunk.css"
+  },
+  {
+    "revision": "2f0ece8123a575531a80",
+    "url": "/o3o-todolist/static/js/2.205b55f8.chunk.js"
+  },
+  {
+    "revision": "18933265addb825ab41b",
+    "url": "/o3o-todolist/static/js/main.4a19971a.chunk.js"
+  },
+  {
+    "revision": "31ef5bca19d3ba0e33b4",
+    "url": "/o3o-todolist/static/js/runtime-main.f8614661.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.615c8025fbd82dafb8844885036ce404.js"
+  "/o3o-todolist/precache-manifest.c3c6a4cabea5d8496ea178dc840fa885.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/o3o-todolist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
